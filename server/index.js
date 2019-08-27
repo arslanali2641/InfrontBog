2defce029ba8eb178fea3ce22c8de8f811f05129
Arslan Ali <arslanali2641@gmail.com>
2019-08-27 12:27:36 +0500

    change the port

--- server/index.js
+++ server/index.js
@@ -57,7 +57,7 @@ app.get("/api/posts", (req, res) => {
 });
 
 //Server Listen
-const PORT = process.env.port || 3000;
+let PORT = process.env.port || 3000;
 app.listen(PORT, () => {
   console.log("Server i working on port" + PORT);
 });
